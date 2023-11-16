acb4cd6d8c4bd94b11687c89faabf2b4c532c82a
Leerlandais <leerlandais@gmail.com>
2023-11-16 13:37:18 +0100

    Working version

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-for (var num = 1; num < 23; num = num + 1){
-    if(num < 10){
+for (var num = 1; num < 21; num = num + 1){
+    if(num <= 10){
         console.log (num);
     }
     else {
-        var numback = 2; console.log(num-numback, "hello"); numback = numback+1;
+        var numback = 19 - num + 1; 
+        console.log(numback);
     }
+    
 }
